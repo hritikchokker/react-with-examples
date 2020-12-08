8c581ec0392a753d6a5d103aa04794e720d34bfa
hritikchokker <hchokker7@gmail.com>
2020-12-09 01:32:42 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import StartRating from "./components/StarRating";
 
 export default function App() {
   return (
-    <StartRating
-      style={{ backgroundColor: "lightblue" }}
-      onDoubleClick={e => alert("you just double clicked")}
-    />
+    <div>
+      <h1> React app explaining basic and advanced concepts</h1>
+      <div>
+        <StartRating
+          style={{ backgroundColor: "lightblue" }}
+          onDoubleClick={e => alert("you just double clicked")}
+        />
+      </div>
+    </div>
   );
 }
