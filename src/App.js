f3856c66e5bd23b0fd0d616e0e9b0bbd632c451b
hritikchokker <hchokker7@gmail.com>
2020-12-16 01:37:50 +0530

    1 file modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Hooks_1 from "./components/hooks_1";
 export default function App() {
   return (
     <div>
       <h1> React app explaining basic and advanced concepts of hooks</h1>
+      <Hooks_1 />
     </div>
   );
 }
