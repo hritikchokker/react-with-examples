1ed70b6506d74175ac9105beae67bf3f7be0e637
hritikchokker <hchokker7@gmail.com>
2020-12-16 01:06:22 +0530

    1 file modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import StartRating from "./components/StarRating";
 
 export default function App() {
   return (
     <div>
-      <h1> React app explaining basic and advanced concepts</h1>
-      <div>
-        <StartRating
-          style={{ backgroundColor: "lightblue" }}
-          onDoubleClick={e => alert("you just double clicked")}
-        />
-      </div>
+      <h1> React app explaining basic and advanced concepts of hooks</h1>
     </div>
   );
 }
