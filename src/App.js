497f251a86e4359a2848323ab66ede7538079236
hritikchokker <hchokker7@gmail.com>
2020-12-25 03:21:31 +0530

    2 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from "react";
+import { UserRepositories } from "./UserRepositories";
+import { Fetch } from "./Fetch";
 
-const tahoe_peaks = [
-  { name: "Freel Peak", elevation: 10891 },
-  { name: "Monument Peak", elevation: 10067 },
-  { name: "Pyramid Peak", elevation: 9983 },
-  { name: "Mt. Tallac", elevation: 9735 }
-];
-
-function List({ data = [], renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item, i) => (
-        <li key={i}>{renderItem(item)}</li>
-      ))}
-    </ul>
+function UserDetails({ data }) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+      <UserRepositories
+        login={data.login}
+        onSelect={repoName => console.log(`${repoName} selected`)}
+      />
+    </div>
+  );
+}
+function GitHubUser({ login }) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSuccess={UserDetails}
+    />
   );
 }
 
 export default function App() {
-  return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={item => (
-        <>
-          {item.name} - {item.elevation.toLocaleString()}
-        </>
-      )}
-    />
-  );
+  return <GitHubUser login="hritikchokker" />;
 }
