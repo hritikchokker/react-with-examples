78cc687ff467a04de6627008188ccd3b248f8527
hritikchokker <hchokker7@gmail.com>
2020-12-25 02:26:19 +0530

    2 files modified, 12 deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
-import Hooks_1 from "./components/hooks_1";
+import React, { useState, useEffect } from "react";
+
+const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
+function GitHubUser({ login }) {
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location
+    });
+  }, [data]);
+
+  useEffect(() => {
+    if (!login) return;
+    if (data && data.login === login) return;
+    fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+      .catch(console.error);
+  }, [login]);
+
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+  return null;
+}
+
 export default function App() {
-  return (
-    <div>
-      <h1> React app explaining basic and advanced concepts of hooks</h1>
-      <Hooks_1 />
-    </div>
-  );
+  return <GitHubUser login="hritikchokker" />;
 }
